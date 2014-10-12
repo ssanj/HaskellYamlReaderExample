c34bac1c3d84027d397d3f58415c9ae22ef3c38d
sanjiv sahayam <sanjsmailbox@gmail.com>
2014-10-12 19:33:27 +1000

    added example files

---
+++ src/Main.hs
@@ -0,0 +1,19 @@
+{-# LANGUAGE DeriveGeneric #-}
+
+import qualified Data.ByteString.Char8 as BS
+import qualified Data.Yaml as Y
+import GHC.Generics
+import Data.Aeson
+
+
+data Cred = Cred { username :: String, password :: String } deriving (Show, Generic)
+
+main :: IO ()
+main = do
+    content <- BS.readFile "config.yml"
+    let parsedContent = Y.decode content :: Maybe Cred
+    case parsedContent of
+        Nothing -> error "Could not parse config file."
+        (Just (Cred u p)) -> putStrLn ("username: " ++ u ++ ", password: " ++ p)
+
+instance FromJSON Cred
